1916a89696cf80e23bbada43aef9e6f0c66781cd
Kostiantyn Stoliarskyi <saintkos117@gmail.com>
2021-06-01 10:49:35 +0300

    Making task

--- src/index.js
+++ src/index.js
@@ -1,11 +1,57 @@
 import './sass/main.scss';
-fetch('https://pokeapi.co/api/v2/pokemon/2')
-  .then(response => {
-    return response.json();
-  })
-  .then(pokemon => {
-    console.log(pokemon);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+
+import { error } from '@pnotify/core/dist/PNotify.js';
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/PNotify.css';
+
+import debounce from 'lodash.debounce';
+
+import fetchCountries from './js/fetchCountries';
+import countriesListMarkup from './templates/countriesList.hbs';
+import countriesCardMarkup from './templates/countriesCard.hbs';
+
+const refs = {
+  input: document.querySelector('.input_country'),
+  countryCard: document.querySelector('.block_card'),
+};
+
+refs.input.addEventListener('input', debounce(onInputSearch, 500));
+
+let inputValue = '';
+
+function onInputSearch(evt) {
+  inputValue = evt.target.value.trim();
+  refs.countryCard.innerHTML = '';
+
+  if (!inputValue) {
+    return;
+  } else {
+    fetchCountries(inputValue)
+      .then(updateNameNotification)
+      .catch(error => console.log(error));
+  }
+}
+
+function addCountriesList(countries) {
+  const markup = countriesListMarkup(countries);
+  refs.countryCard.insertAdjacentHTML('beforeend', markup);
+}
+
+function addCountriesCard(countries) {
+  const markup = countriesCardMarkup(countries[0]);
+  refs.countryCard.insertAdjacentHTML('beforeend', markup);
+}
+
+function updateNameNotification(name) {
+  if (name.length > 10) {
+    error({
+      text: 'Too many matches found. Please enter a more specific query!',
+    });
+    return;
+  }
+  if (name.length >= 2 && name.length <= 10) {
+    addCountriesList(name);
+    return;
+  }
+  addCountriesCard(name);
+}
